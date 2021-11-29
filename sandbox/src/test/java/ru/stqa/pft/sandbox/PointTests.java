aef8897ce432c89077e7d4eb147fe1a0a6a52cd4
Korbi777 <kobyakov20@mail.ru>
2021-11-29 22:28:32 +0400

    Задание №3

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -1,28 +1,33 @@
-//package ru.stqa.pft.sandbox;
-//
-//
-//import org.testng.Assert;
-//import org.testng.annotations.Test;
-//
-//public class PointTests {
-//
-//    @Test
-//    public void testPoints () {
-//        Point a = new Point(0,0,6,2);
-//
-//        Assert.assertEquals(a.distance(), 4);
-//    }
-//
-//    @Test
-//    public void testPoints2 () {
-//        Point a = new Point(1,0,5,2);
-//        double res = a.distance();
-//        Assert.assertNotNull(res, null);
-//    }
-//    @Test
-//    public void testPoints3 () {
-//        Point a = new Point(1,2,3,4);
-//
-//        }
-//}
-//
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests {
+
+
+    @Test
+    public void testPoints() {
+        Point p1 = new Point(1,0);
+        Point p2 = new Point(4,5);
+
+        Assert.assertEquals(p1.distance(p2), 1.4142135623730951);
+    }
+
+    @Test
+    public void testPoints2() {
+        Point p1 = new Point(1,0);
+        Point p2 = new Point(4,5);
+
+        Assert.assertNotNull(p1.distance(p2), null);
+    }
+
+    @Test
+    public void testPoints3() {
+        Point p1 = new Point(1,0);
+        Point p2 = new Point(4,5);
+
+        Assert.assertNull(p1.distance(p2), null);
+    }
+}
+
