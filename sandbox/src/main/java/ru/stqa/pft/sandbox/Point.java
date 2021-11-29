81496f895568e56881904162c5b99eaa5837746f
Korbi777 <kobyakov20@mail.ru>
2021-11-29 21:38:26 +0400

    Задание №2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -4,7 +4,6 @@ class Point {
     public double x;
     public double y;
 
-
     public Point(double x, double y) {
         this.x=x;
         this.y=y;
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Points.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Points.java
@@ -4,7 +4,7 @@ public class Points {
 
     public static void main(String[] args) {
         Point p1 = new Point(-4,3);
-        Point p2 = new Point(3,1);
+        Point p2 = new Point(3,2);
 
         System.out.println("Дистанция между точками: " + "(" + p1.x + ";" + p1.y + ")" + " и " + "(" + p1.x + ";" + p2.y + ")" + " = " + p1.distance(p2));
     }
