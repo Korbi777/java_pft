d3c4a9fac1cdbbc9bdb3acd8b314adfc869a96e6
Korbi777 <kobyakov20@mail.ru>
2021-11-29 21:34:29 +0400

    Задание №2

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -0,0 +1,28 @@
+//package ru.stqa.pft.sandbox;
+//
+//
+//import org.testng.Assert;
+//import org.testng.annotations.Test;
+//
+//public class PointTests {
+//
+//    @Test
+//    public void testPoints () {
+//        Point a = new Point(0,0,6,2);
+//
+//        Assert.assertEquals(a.distance(), 4);
+//    }
+//
+//    @Test
+//    public void testPoints2 () {
+//        Point a = new Point(1,0,5,2);
+//        double res = a.distance();
+//        Assert.assertNotNull(res, null);
+//    }
+//    @Test
+//    public void testPoints3 () {
+//        Point a = new Point(1,2,3,4);
+//
+//        }
+//}
+//
