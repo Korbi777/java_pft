e0f73606e623a5e0c79fcb71bcbe7e2941d09714
Korbi777 <kobyakov20@mail.ru>
2021-12-12 11:58:19 +0400

    Задание 4

--- Adress-web-tests/src/test/java/ru/stqa/pft/addressbook/model/inputDataContact.java
+++ Adress-web-tests/src/test/java/ru/stqa/pft/addressbook/model/inputDataContact.java
@@ -1,5 +1,35 @@
 package ru.stqa.pft.addressbook.model;
 
-public record inputDataContact(String firstname, String middlename, String lastname, String nickname, String title,
-                               String company, String address, String home, String mobile) {
+public class inputDataContact {
+    private final String firstname;
+    private final String middlename;
+    private final String lastname;
+    private final String nickname;
+    private final String title;
+    private final String company;
+    private final String address;
+    private final String home;
+    private final String mobile;
+
+    public inputDataContact (String firstname, String middlename, String lastname, String nickname, String title, String company,
+                      String address, String home, String mobile){
+        this.firstname=firstname;
+        this.middlename=middlename;
+        this.lastname=lastname;
+        this.nickname=nickname;
+        this.title=title;
+        this.company=company;
+        this.address=address;
+        this.home=home;
+        this.mobile=mobile;
+    }
+    public String getFirstname() {return firstname;}
+    public String getMiddlename() {return middlename;}
+    public String getLastname() {return lastname;}
+    public String getNickname() {return nickname;}
+    public String getTitle() {return title;}
+    public String getCompany() {return company;}
+    public String getAddress() {return address;}
+    public String getHome() {return home;}
+    public String getMobile() {return mobile;}
 }
